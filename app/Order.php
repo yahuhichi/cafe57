ae5512cf3b149bf9b0a8c276a14e060db45d6930
hntake <hntake@gmail.com>
2021-12-24 15:16:15 +0900

    Order.php修正20211224

--- app/Order.php
+++ app/Order.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Models;
+// namespace App\Models;
+namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
+    // use HasFactory;
 
     protected $fillable = ['id','product_id','product_name','new_order','staff'];
 }
