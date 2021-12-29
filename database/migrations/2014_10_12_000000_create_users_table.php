67ef56db90af36aceed3b82334836e3c4c47d6ba
haru034 <haru034@icloud.com>
2021-12-29 20:11:30 +0900

    usersテーブルに'user_name'カラムを追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->integer('user_id');
             $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
