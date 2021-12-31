b8fd037fa4194068f7c1fc9a06ea3d7b34dfc1ca
haru034 <haru034@icloud.com>
2021-12-31 19:30:29 +0900

    ホーム画面の簡単なレイアウトを作成

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -6,9 +6,16 @@
     <link rel="stylesheet" href="{{ asset('css/home.css') }}"> <!-- home.cssと連携 -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ホーム画面</title>
 </head>
 <body>
-    
+    <section>
+        <div class="side">
+            <p>サイドバー</p>
+        </div> 
+        <div class="content-1">
+            <p>メインコンテンツ</p>
+        </div>
+    </section>
 </body>
 </html>
