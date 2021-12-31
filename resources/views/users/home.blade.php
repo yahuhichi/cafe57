e35bd50721a665ddce7c9cd80b9ffa036e8b7835
haru034 <haru034@icloud.com>
2021-12-31 14:20:00 +0900

    ホーム画面のbladeファイル作成

---
+++ resources/views/users/home.blade.php
@@ -0,0 +1,14 @@
+<!-- ホーム画面のView -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}"> <!-- home.cssと連携 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
