831d40fa34bee1e64aeb5fe56b8b338adc1a8cad
haru034 <haru034@icloud.com>
2021-12-31 19:32:34 +0900

    新規登録、ログイン、ホーム画面のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,15 @@
 |
 */
 
-// viewを表示する際はget,その他はpost
-
 Route::get('/', function () { // Laravel簡易サーバーを開いた時に最初に表示される画面を表示
     return view('welcome');   // welcome.blade.phpを表示する。
 });
-Route::get('/index', 'UsersController@index')->name('index');
 
-Route::get('/signup_form', 'UsersController@signup_form')->name('signup_form'); // 新規登録画面のviewを表示
-Route::post('/signup', 'UsersController@signup')->name('signup'); 
+Route::get('/signup_form', 'UsersController@signup_form')->name('signup_form'); // 新規登録画面のviewを表示(get)
+Route::post('/signup', 'UsersController@signup')->name('signup'); // 新規登録の処理(post)
 
-Route::get('/login_form', 'UsersController@login_form')->name('login_form'); // ログイン画面のviewを表示
-Route::post('/login', 'UsersController@signup')->name('login'); 
+Route::get('/login_form', 'UsersController@login_form')->name('login_form'); // ログイン画面のviewを表示(get)
+Route::post('/login', 'UsersController@signup')->name('login'); // ログインの処理(post)
+
+Route::get('/home_screen', 'UsersController@home_screen')->name('home_screen'); // ホーム画面のviewを表示(get)
+Route::post('/home', 'UsersController@login')->name('home'); // ホーム画面の処理(post)
