5d3f565585392c820d1b966aff72f9292b8feb2c
haru034 <haru034@icloud.com>
2022-01-05 22:52:46 +0900

    user_idとuser_typeのNULL値を許容

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->integer('user_id')->nullable();
             $table->string('name');
             $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('user_type');
+            $table->integer('user_type')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
