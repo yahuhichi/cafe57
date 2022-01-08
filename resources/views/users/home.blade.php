c354a6e770743225c63208dff6bff8f2fb3fafe2
haru034 <haru034@icloud.com>
2022-01-08 14:30:53 +0900

    サイドバーの追加・持ち出し申請画面、注文申請画面、注文メール送信画面のリンクを削除

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -11,9 +11,18 @@
 <body>
     <section>
         <div class="side">
-            <p>サイドバー</p>
-        </div> 
-        <div class="content-1">
+            <p><h1>cafe57</h1></p>
+            <nav class="sidebar">   
+                <p><a href="{{ url('products') }}"><h3>在庫一覧画面</h3></a></p>
+                <p><a href="{{ url('create') }}">・商品登録画面</a></p> 
+                <p><a href="{{ url('order_table') }}"><h3>注文表画面</h3></a></p>
+                <p><a href="{{ url('') }}"><h3>シフト申請画面</h3></a></p>
+                <p><a href="{{ url('') }}">・シフト管理画面</a></p>
+                <p><a href="{{ url('') }}">・勤怠一覧画面</a></p>
+                <p><a href="{{ route('home_screen') }}">ホーム画面に戻る</a></p>
+            </nav>
+        </div>
+        <div class="home">
             <p>メインコンテンツ</p>
         </div>
     </section>
