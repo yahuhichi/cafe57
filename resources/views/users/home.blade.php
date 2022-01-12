fe140d4b8f11b3188672ae9b2807babb283e0a07
haru034 <haru034@icloud.com>
2022-01-12 19:51:59 +0900

    チャット画面の一部(件名＆メッセージ入力フォーム、投稿ボタン)を作成

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -14,7 +14,7 @@
             <p><h1>cafe57</h1></p>
             <nav class="sidebar">   
                 <p><a href="{{ url('products') }}"><h3>在庫一覧画面</h3></a></p>
-                <p><a href="{{ url('create') }}">・商品登録画面</a></p> 
+                <!-- <p><a href="{{ url('create') }}">・商品登録画面</a></p>  -->
                 <p><a href="{{ url('order_table') }}"><h3>注文表画面</h3></a></p>
                 <p><a href="{{ url('') }}"><h3>シフト申請画面</h3></a></p>
                 <p><a href="{{ url('') }}">・シフト管理画面</a></p>
@@ -23,7 +23,18 @@
             </nav>
         </div>
         <div class="home"> <!-- チャット画面 -->
-            <p><h1>チャット画面</h1></p>
+            <form action="{{ route('home') }}" method="post" autocomplete="off">
+                @csrf <!-- CSRF保護 -->
+                <div class="outer-title-form">
+                    <p><h3>件名</h3></p>
+                    <input type="text" name="title" value="" placeholder="件名を入力してください" autocomplete="off">
+                </div>
+                <div class="outer-message-form">
+                    <p><h3>メッセージ</h3></P>
+                    <textarea name="message" placeholder="ここにメッセージを入力してください" autocomplete="off" rows="30" cols="20"></textarea><br> <!-- rows =「高さ」, cols =「幅」-->
+                    <input type="submit" value="投稿">
+                </div>
+            </form>
         </div>
     </section>
 </body>
