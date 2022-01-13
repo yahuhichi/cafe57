005a0c7bcbb49c64ffc730724dfd4396e34f91f4
haru034 <haru034@icloud.com>
2022-01-13 16:04:20 +0900

    usersテーブルとchatsテーブルを紐づけさせる処理を追加

--- app/Chat.php
+++ app/Chat.php
@@ -9,6 +9,10 @@ class Chat extends Model
     protected $fillable = [
         'user_id', 'title', 'message',
     ];
+    public function user()
+    {
+        return $this->belongsTo('App\User'); // usersテーブルと紐づけ
+    }
 }
 
 
