e17dd08321deb7904cf1703ea6e8b9f0cf46d97c
haru034 <haru034@icloud.com>
2022-01-15 19:33:32 +0900

    サイドバーにログアウトボタンを追加

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -7,15 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ホーム画面</title>
-    <style>
-        div.chatarea {
-            border-style: solid;
-            position: absolute;
-            left: 250px;
-            top: 50px;
-            width: 500px;
-        }
-    </style>
 </head>
 <body>
     <section>
@@ -23,20 +14,22 @@
             <p><h1>cafe57</h1></p>
             <nav class="sidebar">   
                 <p><a href="{{ url('products') }}"><h3>在庫一覧画面</h3></a></p>
-                <!-- <p><a href="{{ url('create') }}">・商品登録画面</a></p>  -->
                 <p><a href="{{ url('order_table') }}"><h3>注文表画面</h3></a></p>
                 <p><a href="{{ url('') }}"><h3>シフト申請画面</h3></a></p>
                 <p><a href="{{ url('') }}">・シフト管理画面</a></p>
                 <p><a href="{{ url('') }}">・勤怠一覧画面</a></p>
                 <p><a href="{{ route('home_screen') }}">ホーム画面に戻る</a></p>
             </nav>
+            <div class="logout_buttom">
+                <input type="submit" value="ログアウト">
+            </div>
         </div>
         <div class="home"> <!-- チャット画面 -->
-            <form action="{{ route('home') }}" method="post" autocomplete="off">
+            <form action="{{ route('chat') }}" method="post" autocomplete="off">
                 @csrf <!-- CSRF保護 -->
                 <div class="outer-title-form">
                     <p><h3>件名</h3></p>
-                    <input type="text" name="title" value="" placeholder="件名を入力してください" autocomplete="off">
+                    <input type="text" name="title" placeholder="件名を入力してください" autocomplete="off" style="height:30px;">
                 </div>
                 <div class="outer-message-form">
                     <p><h3>メッセージ</h3></P>
