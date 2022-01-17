158a6cddb2ade118127b38288de3e28cc8fb38a9
haru034 <haru034@icloud.com>
2022-01-17 18:08:18 +0900

    ログイン認証が完成

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ use App\User; // User.phpを使用
 use Illuminate\Support\Facades\Hash; // パスワードを乱数にする設定
 use Illuminate\Support\Facades\DB; // DBクラスを使用
 use App\Chat; // Chatクラス(chat.php)を使用
+use Illuminate\Support\Facades\Auth; // Authクラスを使用
 
 class UsersController extends Controller
 {
@@ -33,12 +34,18 @@ class UsersController extends Controller
             'name' => 'required',
             'user_name' => 'required',
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'min:8', 'max:255', 'alpha_num']
+            'password' => ['required', 'min:8', 'max:255', 'alpha_num'],
         ]);
 
         // DBインサート(usersテーブルに行を追加)
         $user = new User();
-        $user->fill($request->all())->save(); // fillを使い、モデルの全カラムを更新し、保存。
+        $user->fill([ // fillを使い、モデルの全カラムを更新。
+            'name' => $request->name, 
+            'user_name' => $request->user_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password), // 新規登録時にパスワードをハッシュ化。
+            'user_type' => $request->user_type,
+        ])->save(); // $userに格納されている情報を保存。
 
         //「登録する」ボタンを押すとログイン画面に遷移
         return view('users.login');
@@ -60,11 +67,11 @@ class UsersController extends Controller
             'user_name' => 'required',
             'password' => ['required', 'min:8', 'max:255', 'alpha_num']
         ]);
-        return redirect('home_screen'); // ホーム画面へリダイレクト
-    }
 
-    public function home() // ホーム画面の処理(post)
-    {
-        return redirect('users.login'); // サイドバーの「ログアウト」ボタンを押すとログイン画面へリダイレクト
+        // ログイン認証
+        if(Auth::attempt(['user_name' => $request->input('user_name'), 'password' => $request->input('password')])){
+            return redirect()->route('home_screen');
+            }
+            return redirect()->back();
     }
 }
