eeb07236b4fc61d881dbb253d9f241169942131f
haru034 <haru034@icloud.com>
2022-01-17 18:07:14 +0900

    チャット機能の修正(仮)

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -17,14 +17,29 @@ class ChatController extends Controller
         return view('users.home', ['chats' => $chats]);
     }
 
+    public function logout() // ログアウトの処理(post)
+    {
+        return redirect('users.login'); //「ログアウト」ボタンを押すとログイン画面へリダイレクト
+    }
+
     /**
      * user_id、title、messageの受け渡し
      * 
      */
     public function chat(Request $request)
     {
-        $chats = Chat::orderBy('created_at', 'desc')->get();
-        return view('chat.home_screen', ['chats' => $chats]);
+        $chat = new Chat; // $chatの変数に、Chatモデルを定義
+
+        $chat->user_id = 1; // 仮にuser_idを1とする
+        $chat->title = $request->title; // $chatにtitleをセット
+        $chat->message = $request->message; // $chatにmessageをセット
+
+        $chat->save(); // $chatに格納されている情報を保存。
+
+        // $chats = Chat::orderBy('created_at', 'desc')->get();
+        // return view('chat.home_screen', ['chats' => $chats]);
+
+        return redirect('home_screen'); // ホーム画面にリダイレクト(ChatControllerの14行目のhome_screen関数を呼び出して表示)
     }
 
 }
