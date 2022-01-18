4be3675594e886d035ab005e6d371ae36d540375
haru034 <haru034@icloud.com>
2022-01-18 20:01:23 +0900

    belongsToメソッドを使い、User.phpとのリレーションを記述。

--- app/Chat.php
+++ app/Chat.php
@@ -9,9 +9,10 @@ class Chat extends Model
     protected $fillable = [
         'user_id', 'title', 'message',
     ];
+
     public function user()
     {
-        return $this->belongsTo('App\User'); // usersテーブルと紐づけ
+        return $this->belongsTo('App\User', 'id'); // usersテーブルと紐づけ
     }
 }
 
